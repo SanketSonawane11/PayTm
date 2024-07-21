b64dac8ecd573bccd2ccc6623091898e2f7bc918
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-07-22 02:30:30 +0530

    Installed recoil and added Login and Signup form (only ui and no data storing and no api integration)

---
+++ Frontend/src/app/LoginSignup/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginSignup from '../Components/LoginSignup'
+
+function page() {
+  return (
+    <div className=' flex items-center justify-center w-[100vw] h-[100vh]'>
+        <LoginSignup />
+    </div>
+  )
+}
+
+export default page
--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <Link href='/LoginSignup'> Login </Link>
   )
 }
 
