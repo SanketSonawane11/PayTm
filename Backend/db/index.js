950e535237f39690a9c56016d8a8f02cbfdb1b92
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-07-27 20:48:55 +0530

    Updated for deprecated modules

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -6,7 +6,7 @@ const uri = process.env.URI;
 const connectDb = async () => {
     if (mongoose.connection.readyState === 0) { // Check if already connected
         try {
-            await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+            await mongoose.connect(uri);
             console.log('Connected to MongoDB');
         } catch (err) {
             console.error('Database connection error:', err);
