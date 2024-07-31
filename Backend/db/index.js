6f4ec127dc8398ac664b75535e07b66db7a5d92b
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-07-31 23:37:08 +0530

    MongoDb update

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -5,10 +5,14 @@ const uri = process.env.URI;
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Database connection error:', err);
+        process.exit(1);
     }
 };
 
--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/account", accountRouter);
